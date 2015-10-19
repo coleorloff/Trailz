793f0fb68ba73ecfb90170905d1f69662bd79153
C013 <co790@nyu.edu>
2015-10-19 11:15:13 -0400

    update

--- alchemyscript.js
+++ alchemyscript.js
@@ -1,9 +1,51 @@
+/**
+   Copyright 2014 AlchemyAPI
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+var express = require('express');
+var consolidate = require('consolidate');
+
+var app = express();
+var server = require('http').createServer(app);
+
+//Create the AlchemyAPI object
 var AlchemyAPI = require('./alchemyapi');
 var alchemyapi = new AlchemyAPI();
 
-// var xhr = new XMLHttpRequest();
+// all environments
+app.engine('dust',consolidate.dust);
+app.set('views',__dirname + '/views');
+app.set('view engine', 'dust');
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
 
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
 
+app.get('/', keywords);
+
+var port = process.env.PORT || 3000;
+server.listen(port, function(){
+	console.log('Express server listening on port ' + port);
+	console.log('To view the example, point your favorite browser to: localhost: 3000'); 
+});
 
 
 var fs = require('fs');
@@ -12,32 +54,14 @@ var myText = "I love coffee with milk and sugar. Black coffee is my favorite, \
 but the others are fine too. I know I like it more than tea or milk or soda."
 var myURL = "https://en.wikipedia.org/wiki/Koala"
 
-// console.log(text);
-
 function keywords() {
-	var output = {};
-	alchemyapi.keywords('url', myURL, { 'sentiment':1 }, function(response) {
-		output['keywords'] = { text:text, response:JSON.stringify(response,null,4), results:response['keywords'] };
-		for (var i = 0; i <response['keywords'].length; i++){
-		console.log(response['keywords'][i]['text']);
-		}
-	});
+	// var output = {};
+	// alchemyapi.keywords('url', myURL, { 'sentiment':1 }, function(response) {
+	// 	output['keywords'] = { text:text, response:JSON.stringify(response,null,4), results:response['keywords']['text'] };
+	// });
+	console.log("wow!");
 };
-
 
 keywords();
 
 
-
-
-// alchemyapi.keywords("text", myText, {}, function(response){	
-// 	console.log("status: " + response["status"]);
-// 	console.log("keywords: " + response["keywords"]["text"]);
-// 	console.log("text: " + response["text"]);
-// });
-
-
-// var myText = "Whoa, AlchemyAPI's Node.js SDK is really great, I love my app!";
-// alchemyapi.sentiment("text", myText, {}, function(response) {
-// console.log("Sentiment: " + response["docSentiment"]["type"]);
-// });
